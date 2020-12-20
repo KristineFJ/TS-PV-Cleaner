aa49068cdd14db2c0cece1ddb1fe3414dc0b2079
KristianFJones <me@kristianjones.dev>
2020-12-20 06:16:32 +0000

    feat(Logger): Implement the Logger class I've been using in my projects

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 // src/index.ts
 import { timeout } from './Utils/timeout';
+import { logger, LogMode } from './Library/Logger';
 
 /**
  * Logs a greeting for the name after a 1.5 second delay.
  * @param name User you are greeting
  */
 async function sayHello(name = 'John'): Promise<void> {
-  console.log('Waiting 1.5 seconds then saying Hi');
+  logger.log(LogMode.INFO, 'Waiting 1.5 seconds then saying Hi');
 
   await timeout(1500);
 
-  console.log(`Hello ${name}!`);
+  logger.log(LogMode.INFO, `Hello ${name}!`);
 }
 
-console.log(`Starting TS-Core`);
+logger.log(LogMode.INFO, `Starting TS-Core`);
 
 await sayHello('K-FOSS');
 
